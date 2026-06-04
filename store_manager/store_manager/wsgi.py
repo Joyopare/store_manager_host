aadb1780fc8eafeec2ae201f1d33225ec0f19019
cloud99 <oparejoy05@gmail.com>
2026-06-04 17:29:57 +0000

    Fix Django settings module path for nested project structure

--- store_manager/store_manager/wsgi.py
+++ store_manager/store_manager/wsgi.py
@@ -5,6 +5,6 @@ from django.core.wsgi import get_wsgi_application
 # Add parent directory to Python path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store_manager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store_manager.store_manager.settings')
 application = get_wsgi_application()
 app = application
