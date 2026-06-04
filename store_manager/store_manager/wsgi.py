13025d3f651f119a3854942333ff9441c74f5f72
cloud99 <oparejoy05@gmail.com>
2026-06-04 17:17:13 +0000

    Fix Django module import path and add whitenoise dependency

--- store_manager/store_manager/wsgi.py
+++ store_manager/store_manager/wsgi.py
@@ -1,5 +1,10 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add parent directory to Python path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store_manager.settings')
 application = get_wsgi_application()
 app = application
